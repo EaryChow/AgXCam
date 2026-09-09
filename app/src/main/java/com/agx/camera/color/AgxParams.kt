823fd4f4373301aa70c2ee0fa5bf84746cebdd0d
EaryChow <zhouzijun200047@163.com>
2026-09-09 12:49:08 +0800

    tweak defaults

--- app/src/main/java/com/agx/camera/color/AgxParams.kt
+++ app/src/main/java/com/agx/camera/color/AgxParams.kt
@@ -7,7 +7,7 @@ data class AgxParams(
     val middleGray: Float = 18f,
     val rotation: FloatArray = floatArrayOf(0.22f, -0.021f, -0.053f),
     val attenuation: FloatArray = floatArrayOf(50.0f, 50.0f, 20.0f),
-    val reverseRotation: FloatArray = floatArrayOf(0.120f, -0.021f, -0.053f),
+    val reverseRotation: FloatArray = floatArrayOf(0.22f, -0.021f, -0.053f),
     val purityBoost: FloatArray = floatArrayOf(10.0f, 30.0f, 10.0f),
     val tintingScale: Float = 0f,
     val tintingHue: Float = 0f,
